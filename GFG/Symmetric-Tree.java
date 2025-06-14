760e11dea0091215cdb841380bb61b984ac350e7
Md Tausif Hussain <tausifh21@gmail.com>
2025-06-14 15:46:48 +0530

    Implemented Symmetric Tree check (GFG) on 2025-06-14

---
+++ GFG/Symmetric-Tree.java
@@ -0,0 +1,23 @@
+// Problem: Symmetric Tree
+// Platform: GeeksforGeeks (GFG)
+// Date: 2025-06-14
+// Link: https://practice.geeksforgeeks.org/problems/symmetric-tree/1
+
+class Solution {
+    public boolean isSymmetric(Node root) {
+        // Code here
+        if(root == null){
+            return true;
+        }
+        return chec(root.left, root.right);
+    }
+    static boolean chec(Node l, Node r){
+        if(l==null && r == null){
+            return true;
+        }
+        if(l == null || r == null || l.data != r.data){
+            return false;
+        }
+        return chec(l.left, r.right) && chec(l.right, r.left);
+    }
+}
